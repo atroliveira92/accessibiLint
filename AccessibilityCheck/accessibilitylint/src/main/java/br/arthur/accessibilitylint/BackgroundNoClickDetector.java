71e266c632523994c746f671dcd88477159674ac
atroliveira92 <atroliveira92@gmail.com>
2019-01-17 23:28:11 -0200

    Clear contrast check on BackgroundNoClickDetector

--- AccessibilityCheck/accessibilitylint/src/main/java/br/arthur/accessibilitylint/BackgroundNoClickDetector.java
+++ AccessibilityCheck/accessibilitylint/src/main/java/br/arthur/accessibilitylint/BackgroundNoClickDetector.java
@@ -1,24 +1,15 @@
 package br.arthur.accessibilitylint;
 
-import com.android.annotations.NonNull;
 import com.android.resources.ResourceFolderType;
-import com.android.tools.lint.detector.api.Context;
 import com.android.tools.lint.detector.api.Implementation;
 import com.android.tools.lint.detector.api.Issue;
-import com.android.tools.lint.detector.api.LayoutDetector;
 import com.android.tools.lint.detector.api.LintUtils;
-import com.android.tools.lint.detector.api.Location;
 import com.android.tools.lint.detector.api.ResourceXmlDetector;
-import com.android.tools.lint.detector.api.Speed;
 import com.android.tools.lint.detector.api.XmlContext;
 
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.w3c.dom.Attr;
 import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -26,11 +17,11 @@ import java.util.EnumSet;
 import java.util.List;
 
 import static com.android.SdkConstants.ANDROID_URI;
+import static com.android.SdkConstants.ATTR_BACKGROUND;
 import static com.android.SdkConstants.ATTR_ON_CLICK;
 import static com.android.SdkConstants.FRAME_LAYOUT;
 import static com.android.SdkConstants.LINEAR_LAYOUT;
 import static com.android.SdkConstants.RELATIVE_LAYOUT;
-import static com.android.SdkConstants.REQUEST_FOCUS;
 import static com.android.SdkConstants.VIEW;
 import static com.android.resources.ResourceFolderType.LAYOUT;
 import static com.android.tools.lint.detector.api.Category.A11Y;
@@ -47,6 +38,15 @@ public class BackgroundNoClickDetector extends ResourceXmlDetector {
     public static final Issue ISSUE_NO_CLICK_ON_BACKGROUND_VIEW = Issue.create(
             "NoClickBackgroundView",
              "Avoid make the background view clickable",
+            "Evitar a exigência de realizar toques no plano de fundo do aplicativo para realizar determinada ação, ou seja, fora dos componentes de interface",
+            A11Y,
+            6,
+            WARNING,
+            new Implementation(BackgroundNoClickDetector.class, RESOURCE_FILE_SCOPE));
+
+    public static final Issue ISSUE_BACKGROUND_COLOR_CONTRAST_RATIO = Issue.create(
+            "BackgroundColorContrast",
+            "Avoid make the background view clickable",
             "Evitar a exigência de realizar toques no plano de fundo do aplicativo para realizar determinada ação, ou seja, fora dos componentes de interface",
             A11Y,
             6,
@@ -170,8 +170,9 @@ public class BackgroundNoClickDetector extends ResourceXmlDetector {
         if (hasChildOnClick) {
             context.report(ISSUE_NO_CLICK_ON_BACKGROUND_VIEW, element,
                     context.getValueLocation(attr),
-                    "Avoid make the background view clickable");
+                    "HIHIAvoid make the background view clickable");
         }
     }
+
 
 }
